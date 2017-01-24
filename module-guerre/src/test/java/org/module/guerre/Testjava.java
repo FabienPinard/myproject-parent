9f7139d6ba40f740fcf83095ff8efda10a94ba10
FabienPinard <pinard.fabien@gmail.com>
2017-01-24 14:47:53 +0100

    Pmpom

--- module-guerre/src/test/java/org/module/guerre/Testjava.java
+++ module-guerre/src/test/java/org/module/guerre/Testjava.java
@@ -3,14 +3,13 @@ package org.module.guerre;
 import org.junit.Test;
 
 public class Testjava {
-	
+
 	@Test
 	public void testRien() {
-<<<<<<< HEAD
 		// il se passe quelque chose d'autre patatra
-=======
+
 		// On va dire ça marche
->>>>>>> 7e1daa1a8728d72115dae5d402a99363df86f75f
+
 	}
 
 }
